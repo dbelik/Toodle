38bdd99558b773c8d4cc75480cff57b63e938824
Denis Belik <denis.belik2014@gmail.com>
2021-01-07 00:01:33 +0200

    add loading animation screen while content is loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import anime from "animejs";
 
 function App() {
+  window.addEventListener("load", () => {
+    anime({
+      targets: "#loading-screen",
+      opacity: "0"
+    });
+    anime({
+      targets: "#toodle-root",
+      opacity: "1"
+    });
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button className="button">asdasd</button>
   );
 }
 
